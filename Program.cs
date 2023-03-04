db05a35911b77c49e0e9518620711520e4c1b6a3
Kisekio <dekostump@gmail.com>
2023-03-03 22:50:35 -0500

    Clases generadas, adicion archivo .json

--- Program.cs
+++ Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static System.Console;
+
+LinqQueries queries = new LinqQueries();
+
+ImprimirValores(queries.TodaLaColeccion());
+
+void ImprimirValores(IEnumerable<Book> listaDeLibros)
+{
+    WriteLine("{0,-60} {1,15} {2,15}\n", "Titulo", "N. Paginas", "Fecha de publicacion");
+    foreach (var item in listaDeLibros)
+    {
+        WriteLine("{0,-60} {1,15} {2,15}", item.Title, item.PageCount, item.PublishedDate.ToShortDateString());
+    }
+} 
